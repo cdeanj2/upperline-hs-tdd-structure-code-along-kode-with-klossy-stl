3afb477b3d6c3a7199a662d139c36a1c61d63f5a
Caroline Jordan <cdeanj2@gmail.com>
2016-06-22 15:25:23 +0000

    Done.

---
+++ my_code.rb
@@ -0,0 +1,15 @@
+def shout
+  "WAHOO"
+end
+
+def shout_word(something)
+  "#{something}!!!".upcase
+end
+
+def square(n)
+  n*n
+end
+
+def concatenate_strings(word1,word2)
+  "#{word1} #{word2}"
+end
